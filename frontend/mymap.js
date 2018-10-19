a561da836727b7aef033bdf282f36b86354318e7
Carsten <kessler@plan.aau.dk>
2018-10-19 13:07:02 +0200

    Click + jQuery included

--- frontend/mymap.js
+++ frontend/mymap.js
@@ -1,9 +1,14 @@
 var mymap = L.map('map')
-               .setView([55.676111, 12.568333], 13)
-             .addLayer(L.tileLayer(
-               'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                  maxZoom: 18,
-                  attribution: 'Map data © \
+  .setView([55.676111, 12.568333], 13)
+  .addLayer(L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      attribution: 'Map data © \
                   <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
-                })
-            );
+    }));
+
+function onMapClick(e) {
+  alert("You clicked the map at " + e.latlng);
+}
+
+mymap.on('click', onMapClick);
