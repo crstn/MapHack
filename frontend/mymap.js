f92d8586dc81563bad558eb99981f8637d891aa1
Carsten <kessler@plan.aau.dk>
2018-10-19 12:58:00 +0200

    Firdt version of frontend

---
+++ frontend/mymap.js
@@ -0,0 +1,9 @@
+var mymap = L.map('map')
+               .setView([55.676111, 12.568333], 13)
+             .addLayer(L.tileLayer(
+               'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                  maxZoom: 18,
+                  attribution: 'Map data © \
+                  <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+                })
+            );
